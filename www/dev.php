7dfe94f8ac5bc0d44c7c0cc59bd3a19e58d0be8a
Simon Asika <asika32764@gmail.com>
2015-03-03 10:33:45 +0800

    Add access check

--- www/dev.php
+++ www/dev.php
@@ -6,23 +6,6 @@
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
-// For dev environment
-$allowRemotes = array(
-	'127.0.0.1',
-	'fe80::1',
-	'::1',
-	'219.68.97.4',
-	'220.135.84.82'
-);
-
-if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-	|| !(in_array(@$_SERVER['REMOTE_ADDR'], $allowRemotes)))
-{
-	header('HTTP/1.0 403 Forbidden');
-
-	exit('Forbidden');
-}
-
 // Start composer
 $autoload = __DIR__ . '/../vendor/autoload.php';
 
@@ -36,6 +19,25 @@ include_once $autoload;
 include_once __DIR__ . '/../etc/define.php';
 
 $app = new Windwalker\Web\DevApplication(\Windwalker\Ioc::factory());
+
+// For dev environment
+$allowRemotes = array(
+	'127.0.0.1',
+	'fe80::1',
+	'::1',
+);
+
+$allowRemotes = array_merge($allowRemotes, $app->get('system.allow_ips'));
+
+show($allowRemotes);
+
+if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+	|| !(in_array(@$_SERVER['REMOTE_ADDR'], $allowRemotes)))
+{
+	header('HTTP/1.0 403 Forbidden');
+
+	exit('Forbidden');
+}
 
 define('WINDWALKER_DEBUG', $app->get('system.debug'));
 

c49e1290a38bd7ab9b6868917a28c1c26cbeb54e
Simon Asika <asika32764@gmail.com>
2015-04-13 15:23:36 +0800

    Use our own session path

--- src/User/Listener/UserListener.php
+++ src/User/Listener/UserListener.php
@@ -49,9 +49,27 @@ class UserListener
 	 */
 	public function onAfterInitialise(Event $event)
 	{
-		if (isset($_COOKIE[session_name()]))
-		{
-			session_id($_COOKIE[session_name()]);
-		}
+//		if (isset($_COOKIE[session_name()]))
+//		{
+//			session_id($_COOKIE[session_name()]);
+//		}
+
+		// @see http://natesilva.tumblr.com/post/250569350/php-sessions-timeout-too-soon-no-matter-how-you
+
+		// Session lifetime of 3 hours
+		// ini_set('session.gc_maxlifetime', 10800);
+
+		// Enable session garbage collection with a 1% chance of
+		// running on each session_start()
+		ini_set('session.gc_probability', 1);
+		ini_set('session.gc_divisor', 100);
+
+		/*
+		 * Our own session save path; it must be outside the
+		 * default system save path so Debian's cron job doesn't
+		 * try to clean it up. The web server daemon must have
+		 * read/write permissions to this directory.
+		 */
+		session_save_path(WINDWALKER_ROOT . '/sessions');
 	}
 }
